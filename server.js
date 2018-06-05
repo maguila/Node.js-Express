a7f4ddf46c3b31a1b231fd7a0caaff9eda62c649
Miguel Aguila <m.aguila@solcloud.cl>
2018-06-05 19:13:52 -0400

    fix gethero para un solo registro

--- server.js
+++ server.js
@@ -3,32 +3,44 @@ var app = express();
 var cors = require('cors');
 app.use(cors());
 
-
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/demo');
 
-const Persona = mongoose.model('Persona', { nombre: String , edad: Number });
-const Heroes = mongoose.model('Heroes', { id: Number , name: String });
+const Hero    = mongoose.model('Heroes', {name: String});
+const Persona = mongoose.model('Personas', {nombre: String, apellido: String});
 
 
 app.get("/all", function(req, res){
   console.log("hola rest");
   res.setHeader("Content-Type","application/json");
   Persona.find(function(err, doc){
-      console.log("find ..." + doc[1].nombre.toUpperCase());
+      //console.log("find ..." + doc[1].name.toUpperCase());
       res.send(doc);
-  });
+  }); 
 });
 
 app.get("/heroes", function(req, res){
   console.log("hola rest");
   res.setHeader("Content-Type","application/json");
 
-  Heroes.find(function(err, doc){
+  Hero.find(function(err, doc){
       console.log("find ..." + doc[0].name.toUpperCase());
       res.send(doc);
   });
+});
 
+app.get("/heroes/:id", function(req, res){
+  var idParam = parseInt(req.params.id);
+
+  console.log("hola rest /heroes .. " + idParam);
+  res.setHeader("Content-Type","application/json");
+  Hero.findOne({id:idParam},function(err, doc){
+    res.send(doc);
+  });
+});
+
+app.post("/guardar", function(req, res){
+  res.send("post response");
 });
 
 app.post("/guardar", function(req, res){
