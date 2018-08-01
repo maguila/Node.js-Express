b416a5fd82c4601e9fed8b02ebaa3d5d705a2403
Miguel Aguila <m.aguila@solcloud.cl>
2018-08-01 18:13:19 -0400

    post con update, se instala body-parser

--- redail.js
+++ redail.js
@@ -1,7 +1,16 @@
 var express = require("express");
-var app = express();
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/redail');
@@ -17,6 +26,13 @@ app.get("/all", function (req, res) {
         res.send(doc);
     });
 });
+
+app.post("/update/propiedad", function (req, res) {
+    //console.log(Object.keys(req))
+    console.log("hola redail rest [POST] .... " + req.body);     
+    console.log("hola redail rest [KEYS] .... " + JSON.stringify(req.body, null, 2) );
+});
+
 
 app.listen(3000, function () {
     console.log(" iniciando servidor ...");
