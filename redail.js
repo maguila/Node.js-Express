80bfe0a7c62eb9ea82f9be678161e64b205b3e70
Miguel Aguila <m.aguila@solcloud.cl>
2018-08-01 19:03:42 -0400

    cambios update set()

--- redail.js
+++ redail.js
@@ -1,25 +1,20 @@
 var express = require("express");
 var cors = require('cors');
 var bodyParser = require('body-parser');
-
 var app = express();
 
-
-app.use(cors());
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
+app.use(cors()); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse application/json
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/redail');
 
-const Propiedades = mongoose.model('Propiedades', {  });
+const Propiedades = mongoose.model('Propiedades', { tipo: String, direccion: String });
 
 
 app.get("/all", function (req, res) {
-    console.log("hola redail rest");
+    console.log("hola redail rest ... get propiedades");
     res.setHeader("Content-Type", "application/json");
     Propiedades.find(function (err, doc) {
         //console.log("find ..." + doc[1].name.toUpperCase());
@@ -31,6 +26,28 @@ app.post("/update/propiedad", function (req, res) {
     //console.log(Object.keys(req))
     console.log("hola redail rest [POST] .... " + req.body);     
     console.log("hola redail rest [KEYS] .... " + JSON.stringify(req.body, null, 2) );
+    console.log("ID = " + req.body._id);
+
+    Propiedades.findById(req.body._id, function(err, obj){        
+        
+        if (err) return handleError(err);
+
+        //Es mejor usar set()
+        //obj.tipo = req.body.tipo;
+        //obj.direccion = req.body.direccion;
+        obj.set(req.body);
+
+        console.log("save ... req.body.tipo = " + req.body.tipo);
+        console.log("save ... objeto id=" + obj._id);
+        console.log("save ... objeto tipo=" + obj.tipo);
+
+        obj.save(function(err, updatedObj){
+            if (err) return handleError(err);
+
+            console.log("actualizado correctamente ... updatedObj.tipo=" + updatedObj.tipo + "\n");
+            res.send(updatedObj);            
+        });         
+    });    
 });
 
 
